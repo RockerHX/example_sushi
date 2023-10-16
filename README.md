13af4507cf1a1f0a5c42f4d7c7a477e6c027a2b5
RockerHX <rockerhx@gmail.com>
2023-10-16 15:27:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# example_sushi
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/example_sushi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.example_sushi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
